b57f06c1371983650cf398327277f6cb690c30f8
Shubham Kushwaha <41871310+kushwahashubham@users.noreply.github.com>
2023-08-27 19:45:24 +0530

    Add files via upload
    all sections are done

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -1,6 +1,13 @@
+import Servicecard from "../Components/Servicecard";
+import { services } from "../constants";
+
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <Servicecard key={service.label} {...service} />
+      ))}
+    </section>
   )
 }
 
